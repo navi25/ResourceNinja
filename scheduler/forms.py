1b7cc2126654f24483e5c737e1c5b93a72d1ac0a
Navendra Jha <navendra@asu.edu>
2018-04-28 13:00:24 -0700

    Added RequestForm and related views

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -4,3 +4,14 @@ class TestForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
     age = forms.IntegerField(label="Your Age")
     about_me = forms.CharField(label="About Me", max_length=300)
+
+class RequestForm(forms.Form):
+    firstName = forms.CharField(label='First Name', max_length=100)
+    lastName = forms.CharField(label='Last Name', max_length=100)
+    dateSubmitted = forms.DateField(label='Date Submitted (dd/mm/xxxx)')
+    datePartsNeeded =  forms.DateField(label='Date Parts Needed (dd/mm/xxxx)')
+    facultyAdvisor = forms.CharField(label='Professor or ASU Acct Rep that will be approving the purchase')
+    paymentAccountNo = forms.IntegerField(label='ASU Acct number to be used for payment')
+    className = forms.CharField(label='Class Name')
+    machineRequested = forms.CharField(label='Machine Requested')
+    listParts = forms.CharField(label='List of Part Names and Quantities')
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import TestForm
+from .forms import TestForm, RequestForm
 from django.http import HttpResponseRedirect
 from .models import CustomerModel, MachineModel
 from django.views import generic
@@ -9,17 +9,27 @@ def submit_form(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = TestForm(request.POST)
+        form = RequestForm(request.POST)
         # check whether it's valid:
+
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
+            data = form.cleaned_data
+            print(data)
+            firstName = data['firstName']
+            lastName = data['lastName']
+            dateSubmitted = data['dateSubmitted']
+            datePartsNeeded =  data['datePartsNeeded']
+            facultyAdvisor = data['facultyAdvisor']
+            paymentAccountNo = data['paymentAccountNo']
+            className = data['className']
+            machineRequested = data['machineRequested']
+            listParts = data['listParts']
             # redirect to a new URL:
             return HttpResponseRedirect('success')
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = TestForm()
+        form = RequestForm()
 
     return render(request, 'baseform.html', {'form': form})
 
